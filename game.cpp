c0bea22e6d79873e8e6ffcd56bbcf6b7266dd9f8
yusrababari <yusra.babari@outlook.com>
2026-06-01 21:06:14 +0100

    Implement Number Guesser Game logic
    Fix syntax errors and improve output messages.

---
+++ game.cpp
@@ -0,0 +1,17 @@
+//Number Guesser Game
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    cout << "Number Guesser Game:" << end1;
+    int answer=rand()
+    int numberEntered;
+    cout << "Type a number:";
+    cin >> x;
+    if (x == answer){
+        cout << "You have guessed correctly!" << end1;
+    }   cout << "Incorrect:Try Again" << end1;
+    
+}
